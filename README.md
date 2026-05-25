312b5042bd275e533746e663299cb8327314807a
Elpataina <chiaramanc@yahoo.it>
2026-05-25 18:04:25 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+#  Dajelingo
+
+# Sito web sui dialetti italiani
+
+# Captcha su domande per veri italiani
+
+# Proverbio casuale dell'italia
+
+# Chi sono, da dove vengo
+
+# Problema: fatta l'italia, ma non gli italiani
+
+# Sezione traduttore per dialetti
+
+# Esercizi di traduzione per dialetti vocali
---
+++ backend.py
@@ -0,0 +1,14 @@
+import random
+
+proverbi = ["Abbasso l'Inter, viva il Milan",
+             "Non è tutto oro quel che luccica, ma il venditore alle bancarelle ti dirà il contrario",
+            "L'erba del vicino è sempre più verde, quindi ho preso il tosaerba e sono andato a tagliargliela",
+            "Chi nasce tondo non muore quadrato, infatti io sono un ipercubo quadrimensionale",
+            "Non ho paura di chi mette l'ananas sulla pizza, ma di chi mette la pizza sull'ananas",
+            "I soldi non fanno la felicità, ma la pizza sì: quindi com'è possibile che per comprare la pizza servano i soldi??",
+            "Tra i due litiganti il terzo esplode",
+            "Chi semina vento viola le leggi della fisica",]
+
+proverbio = random.choice(proverbi)
+
+print(proverbio)
